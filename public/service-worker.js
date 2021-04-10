39ab2f35119f8c8f82059b6081d2fc7860e05ef7
Horolsky <horolsky89@outlook.com>
2021-04-10 12:30:12 +0300

    version to 0.2.6 upd

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,7 +17,7 @@
    "/precache-manifest.42eaa1d67bcc35eee4e1620e31803da3.js"
  );
  
- var cacheName = 'sectio-canonis-v0.2.5';
+ var cacheName = 'sectio-canonis-v0.2.6';
  
  self.addEventListener('install', function (event) {
    event.waitUntil(
